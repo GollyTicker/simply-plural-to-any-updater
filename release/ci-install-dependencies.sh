8940c88fed7f40bb10aebc7a0e40441b40da1c55
GollyTicker <golly.ticker@gmail.com>
2025-08-26 03:17:33 +0200

    ci fix.

--- release/ci-install-dependencies.sh
+++ release/ci-install-dependencies.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y gcc-mingw-w64-x86-64 gcc-mingw-w64-i686
 # patch discord-rich-presence
 echo "Patching discord-rich-presence..."
 rm -rf patched/discord-rich-presence
-git clone git@github.com:vionya/discord-rich-presence patched/discord-rich-presence
+git clone https://github.com/vionya/discord-rich-presence patched/discord-rich-presence
 ( cd patched/discord-rich-presence && git -c advice.detachedHead=false checkout 0.2.5 )
 patch patched/discord-rich-presence/src/activity.rs < patched/activity.rs.patch
 patch patched/discord-rich-presence/src/discord_ipc.rs < patched/discord_ipc.rs.patch
