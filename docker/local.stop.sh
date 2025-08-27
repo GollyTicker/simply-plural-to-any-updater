650d5c31d8f7fffd9f3310b94d45685cb01b068c
GollyTicker <golly.ticker@gmail.com>
2025-08-28 01:00:17 +0200

    fix

--- docker/local.stop.sh
+++ docker/local.stop.sh
@@ -4,10 +4,9 @@ set +e
 
 docker compose -f docker/local.compose.yml stop
 
+docker logs sp2any-db > docker/logs/sp2any-db.log 2>&1
 docker logs sp2any-api > docker/logs/sp2any-api.log 2>&1
 docker logs sp2any-frontend > docker/logs/sp2any-frontend.log 2>&1
-
-docker logs sp2any-db > docker/logs/sp2any-db.log 2>&1
 
 docker compose -f docker/local.compose.yml down --volumes --remove-orphans
 
