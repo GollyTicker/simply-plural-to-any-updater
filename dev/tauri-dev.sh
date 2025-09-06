1df500813dfcc9f5af58e1f521c3633cbf5d181b
GollyTicker <golly.ticker@gmail.com>
2025-09-06 03:27:33 +0200

    rename src-tauri to bridge-src-tari

--- dev/tauri-dev.sh
+++ dev/tauri-dev.sh
@@ -2,4 +2,16 @@
 
 set -euo pipefail
 
+export TAURI_APP_PATH="bridge-src-tauri"
+
+force_vite() {
+    sleep 1.5s
+    echo "Forcing vite to render..."
+    curl --no-progress-meter http://localhost:5173 >/dev/null
+    echo "Ok."
+}
+
+
+force_vite &
 cargo tauri dev
+
