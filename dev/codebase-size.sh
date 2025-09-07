128eef653db3460befb4d9b1c994508b5799cc3d
GollyTicker <golly.ticker@gmail.com>
2025-09-08 01:50:27 +0200

    Quality.

--- dev/codebase-size.sh
+++ dev/codebase-size.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 echo "===== CLOC ===="
-cloc --exclude-dir=node_modules,target --exclude-ext=json .
+cloc --exclude-dir=node_modules,target --exclude-ext=json,svg .
 
 echo "===== Largest Rust files by LoC ==="
 ls src/*/* | xargs wc -l | sort -n -r | tail -n +2 | head -n 10
