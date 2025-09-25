c96342463660498a352a7f112f583b65b192b25d
GollyTicker <golly.ticker@gmail.com>
2025-09-25 16:41:26 +0200

    ui: add separators in configs for better visuals

--- frontend/src/components/Config.vue
+++ frontend/src/components/Config.vue
@@ -4,74 +4,102 @@
     <form @submit.prevent="saveConfigAndRestart" autocomplete="off">
       <button type="submit">Save and Restart</button>
       <p id="config-update-status">{{ status }}</p>
-      <div class="config-grid">
-        <div class="config-item">
-          <label for="wait_seconds">Wait Seconds</label>
-          <input id="wait_seconds" type="number" v-model.number="config.wait_seconds" />
-        </div>
-        <div class="config-item">
-          <label for="system_name">System Name</label>
-          <input id="system_name" type="text" v-model="config.system_name" />
-        </div>
-        <div class="config-item">
-          <label for="status_prefix">Status Prefix</label>
-          <input id="status_prefix" type="text" v-model="config.status_prefix" />
-        </div>
-        <div class="config-item">
-          <label for="status_no_fronts">Status No Fronts</label>
-          <input id="status_no_fronts" type="text" v-model="config.status_no_fronts" />
-        </div>
-        <div class="config-item">
-          <label for="status_truncate_names_to">Status Truncate Names To</label>
-          <input id="status_truncate_names_to" type="number" v-model.number="config.status_truncate_names_to" />
-        </div>
-        <div class="config-item">
-          <label for="enable_discord">Enable Discord</label>
-          <input id="enable_discord" type="checkbox" v-model="config.enable_discord" />
-        </div>
-        <div class="config-item">
-          <label for="enable_discord_status_message">Enable Discord Status Message</label>
-          <input id="enable_discord_status_message" type="checkbox" v-model="config.enable_discord_status_message" />
-        </div>
-        <div class="config-item">
-          <label for="enable_vrchat">Enable VRChat</label>
-          <input id="enable_vrchat" type="checkbox" v-model="config.enable_vrchat" />
-        </div>
-        <div class="config-item">
-          <label for="simply_plural_token">Simply Plural Token</label>
-          <input id="simply_plural_token" type="password" :value="config.simply_plural_token?.secret"
-            @input="setSecret('simply_plural_token', $event)" />
-        </div>
-        <div class="config-item">
-          <label for="discord_status_message_token">Discord Status Message Token</label>
-          <input id="discord_status_message_token" type="password" :value="config.discord_status_message_token?.secret"
-            @input="setSecret('discord_status_message_token', $event)" />
-        </div>
-        <div class="config-item">
-          <label for="vrchat_username">VRChat Username</label>
-          <input id="vrchat_username" type="password" :value="config.vrchat_username?.secret"
-            @input="setSecret('vrchat_username', $event)" />
-        </div>
-        <div class="config-item">
-          <label for="vrchat_password">VRChat Password</label>
-          <input id="vrchat_password" type="password" :value="config.vrchat_password?.secret"
-            @input="setSecret('vrchat_password', $event)" />
-        </div>
-        <div class="config-item">
-          <button @click.prevent="loginToVRChat">Login to VRChat</button>
-        </div>
-        <div class="config-item">
-          <label for="vrchat_2fa_code">VRChat 2FA Code</label>
-          <input id="vrchat_2fa_code" type="text" v-model="vrchatTwoFactor" />
-          <button @click.prevent="submitVRChat2FA">Submit 2FA</button>
-        </div>
-        <p id="vrchat-login-status">{{ vrchatLoginStatus }}</p>
-        <div class="config-item">
-          <label for="vrchat_cookie">VRChat Cookie</label>
-          <input id="vrchat_cookie" type="password" :value="config.vrchat_cookie?.secret"
-            @input="setSecret('vrchat_cookie', $event)" />
+      <div class="config-section">
+        <h2>General</h2>
+        <div class="config-grid">
+          <div class="config-item">
+            <label for="wait_seconds">Wait Seconds</label>
+            <input id="wait_seconds" type="number" v-model.number="config.wait_seconds" />
+          </div>
         </div>
       </div>
+      <div class="config-section">
+        <h2>Simply Plural</h2>
+        <div class="config-grid">
+          <div class="config-item">
+            <label for="simply_plural_token">Simply Plural Token</label>
+            <input id="simply_plural_token" type="password" :value="config.simply_plural_token?.secret"
+              @input="setSecret('simply_plural_token', $event)" />
+          </div>
+          <div class="config-item">
+            <label for="system_name">System Name</label>
+            <input id="system_name" type="text" v-model="config.system_name" />
+          </div>
+        </div>
+      </div>
+      <div class="config-section">
+        <h2>Status Message</h2>
+        <div class="config-grid">
+          <div class="config-item">
+            <label for="status_prefix">Status Prefix</label>
+            <input id="status_prefix" type="text" v-model="config.status_prefix" />
+          </div>
+          <div class="config-item">
+            <label for="status_no_fronts">Status No Fronts</label>
+            <input id="status_no_fronts" type="text" v-model="config.status_no_fronts" />
+          </div>
+          <div class="config-item">
+            <label for="status_truncate_names_to">Status Truncate Names To</label>
+            <input id="status_truncate_names_to" type="number" v-model.number="config.status_truncate_names_to" />
+          </div>
+        </div>
+      </div>
+      <div class="config-section">
+        <h2>Discord</h2>
+        <div class="config-grid">
+          <div class="config-item">
+            <label for="enable_discord">Enable Discord</label>
+            <input id="enable_discord" type="checkbox" v-model="config.enable_discord" />
+          </div>
+          <!-- not shown currently until it's properly had it's warnings added
+          <div class="config-item">
+            <label for="enable_discord_status_message">Enable Discord Status Message</label>
+            <input id="enable_discord_status_message" type="checkbox" v-model="config.enable_discord_status_message" />
+          </div>
+          <div class="config-item">
+            <label for="discord_status_message_token">Discord Status Message Token</label>
+            <input id="discord_status_message_token" type="password"
+              :value="config.discord_status_message_token?.secret"
+              @input="setSecret('discord_status_message_token', $event)" />
+          </div>
+          -->
+        </div>
+      </div>
+      <div class="config-section">
+        <h2>VRChat</h2>
+        <div class="config-grid">
+          <div class="config-item">
+            <label for="enable_vrchat">Enable VRChat</label>
+            <input id="enable_vrchat" type="checkbox" v-model="config.enable_vrchat" />
+          </div>
+          <div class="config-item">
+            <label for="vrchat_username">VRChat Username</label>
+            <input id="vrchat_username" type="password" :value="config.vrchat_username?.secret"
+              @input="setSecret('vrchat_username', $event)" />
+          </div>
+          <div class="config-item">
+            <label for="vrchat_password">VRChat Password</label>
+            <input id="vrchat_password" type="password" :value="config.vrchat_password?.secret"
+              @input="setSecret('vrchat_password', $event)" />
+          </div>
+          <div class="config-item">
+            <button @click.prevent="loginToVRChat">Login to VRChat</button>
+          </div>
+          <div class="config-item">
+            <label for="vrchat_2fa_code">VRChat 2FA Code</label>
+            <input id="vrchat_2fa_code" type="text" v-model="vrchatTwoFactor" />
+            <button @click.prevent="submitVRChat2FA">Submit 2FA</button>
+          </div>
+          <p id="vrchat-login-status">{{ vrchatLoginStatus }}</p>
+          <div class="config-item">
+            <label for="vrchat_cookie">VRChat Cookie</label>
+            <input id="vrchat_cookie" type="password" :value="config.vrchat_cookie?.secret"
+              @input="setSecret('vrchat_cookie', $event)" />
+          </div>
+        </div>
+      </div>
+      <button type="submit">Save and Restart</button>
+      <p id="config-update-status-2">{{ status }}</p>
     </form>
   </div>
 </template>
@@ -173,6 +201,18 @@ onMounted(async () => {
 .config-container {
   padding: 2rem;
   font-family: sans-serif;
+}
+
+.config-section {
+  margin-top: 1rem;
+  border-top: 1px solid #ccc;
+  padding-top: 0.5rem;
+}
+
+.config-section:last-of-type {
+  margin-bottom: 1rem;
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 0.5rem;
 }
 
 .config-grid {
