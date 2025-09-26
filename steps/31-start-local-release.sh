89f7917549ef4d77a174085bd03fe7176c87176a
GollyTicker <golly.ticker@gmail.com>
2025-09-26 18:27:54 +0200

    Revert "Inject SP2ANY_BASE_URL to frontend during startup-time."
    This reverts commit 4dbc218d22d0fde33dbdccfa7a8c16410dd5ccfb.

--- steps/31-start-local-release.sh
+++ steps/31-start-local-release.sh
@@ -5,20 +5,17 @@ set -euo pipefail
 export DISCORD_STATUS_MESSAGE_UPDATER_AVAILABLE=false
 ENABLE_DISCORD_STATUS_MESSAGE=false
 
-export SP2ANY_BASE_URL="http://localhost:23327"
-
 main() {
-    rm -rf output/sp2any-frontend || true
     mkdir -p output/sp2any-frontend
     tar -xzf output/sp2any-frontend.tar.gz -C output/sp2any-frontend/
-    sed -i "s|__SP2ANY_BASE_URL__|${SP2ANY_BASE_URL}|g" output/sp2any-frontend/assets/*.js
-    echo "Frontend substitution: SP2ANY_BASE_URL=$SP2ANY_BASE_URL"
 
     start_backend
     
     echo "Showing logs... Abort with ^C to stop backend."
     docker logs -f sp2any-api
 }
+
+export BASE_URL="http://localhost:8080"
 
 start_backend() {
     echo "start_backend"
