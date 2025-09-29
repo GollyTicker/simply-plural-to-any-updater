25f49a77d35e99d74e38b88cc4cc6320cf9df309
GollyTicker <golly.ticker@gmail.com>
2025-09-29 23:38:11 +0200

    .

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
-    // sqlx: trigger recompilation when a new migration is added
+    // sqlx: trigger recompilation when a new migration is added or the license text has changed
     println!("cargo:rerun-if-changed=docker/migrations");
+    println!("cargo:rerun-if-changed=docker/license*");
 }
