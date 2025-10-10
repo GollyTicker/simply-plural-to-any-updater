de0b5885aab575be7487c3cbec8e2469ea56dd01
Ayake <golly.ticker@gmail.com>
2025-10-10 14:56:18 +0200

    append event

--- steps/40-get-sp-events.sh
+++ steps/40-get-sp-events.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-ssh ayake.net "docker logs sp2any-api-public-test 2>&1 | grep 'SP WS payload' | grep -v 'Authentication violation' | grep -v \")': {}\" | grep -v 'Successfully authenticated'" > sp-events.log
+ssh ayake.net "docker logs sp2any-api-public-test 2>&1 | grep 'SP WS payload' | grep -v 'Authentication violation' | grep -v \")': {}\" | grep -v 'Successfully authenticated'" >> sp-events.log
 
